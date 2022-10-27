7a79de80b2d9ddb3b00797944ade65ccb93a0254
mr-pires <piresrodriguesmiguel@gmail.com>
2022-10-27 15:57:28 +0100

    user names & avatars

--- src/components/IssueItem.jsx
+++ src/components/IssueItem.jsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
 import { GoIssueClosed, GoComment } from 'react-icons/go';
 import { relativeDate } from '../helpers/relativeDate';
+import { useUserData } from "../helpers/useUserData";
 
 export const IssueItem = ({ key, title, number, assignee, commentCount, createdBy, createdDate, labels, status }) => {
+  const assigneeUser = useUserData(assignee)
+  const createdByUser = useUserData(createdBy)
+
   return (
     <li key={key}>
-      <div>
+      <div> 
         {status === 'done' || status === 'canceled' ? (
           <GoIssueClosed style={{ color: 'red' }} />
         ) : (
@@ -25,10 +29,10 @@ export const IssueItem = ({ key, title, number, assignee, commentCount, createdB
           ))}
         </span>
         <small>
-          #{number} opened {relativeDate(createdDate)} by {createdBy}
+          #{number} opened {relativeDate(createdDate)} {createdByUser.isSuccess ? `by ${createdByUser.data.name}` : ''}
         </small>
       </div>
-      {assignee ? <div>{assignee}</div> : null}
+      {assignee ? <img src={assigneeUser.isSuccess ? assigneeUser.data.profilePictureUrl : ""}  className="assigned-to" alt={`Assigned to ${assigneeUser.isSuccess ? assigneeUser.data.name : "avatar"}`} /> : null}
       <span className='comment-count'>
         {commentCount ? (
           <>
