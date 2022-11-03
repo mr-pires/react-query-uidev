b1cedfc1b8855fee9407eebfe937c8c1a42ab238
mr-pires <piresrodriguesmiguel@gmail.com>
2022-11-03 10:07:05 +0000

    Issue page

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -1,35 +1,37 @@
 import { useQuery } from "react-query";
-import { GoIssueOpened } from 'react-icons/go'
 import { IssueItem } from "./IssueItem";
 
-export default function IssuesList({labels, status}) {
-  const issuesQuery = useQuery(
-    ['issues', {labels, status}],
-    () => {
-      const statusString = status ? `&status=${status}` : ""
-      const labelsString = labels.map((label) => `labels[]=${label}`).join("&")
-      return fetch(`/api/issues?${labelsString}${statusString}`).then(res => res.json())
-    }
-  )
+export default function IssuesList({ labels, status }) {
+  const issuesQuery = useQuery(["issues", { labels, status }], () => {
+    const statusString = status ? `&status=${status}` : "";
+    const labelsString = labels.map((label) => `labels[]=${label}`).join("&");
+    return fetch(`/api/issues?${labelsString}${statusString}`).then((res) =>
+    res.json()
+    );
+  });
+    
   return (
     <div>
       <h2>Issues List</h2>
-      {issuesQuery.isLoading
-        ? <p>Loading...</p>
-        : <ul className="issues-list">
-          {issuesQuery.data.map(issue => <IssueItem
-            key={issue.id}
-            title={issue.title}
-            number={issue.number}
-            assignee={issue.assignee}
-            commentCount={issue.comments.length}
-            createdBy={issue.createdBy}
-            createdDate={issue.createdDate}
-            labels={issue.labels}
-            status={issue.status}
-          />)}
+      {issuesQuery.isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul className="issues-list">
+          {issuesQuery.data.map((issue) => (
+            <IssueItem
+              key={issue.id}
+              title={issue.title}
+              number={issue.number}
+              assignee={issue.assignee}
+              commentCount={issue.comments.length}
+              createdBy={issue.createdBy}
+              createdDate={issue.createdDate}
+              labels={issue.labels}
+              status={issue.status}
+            />
+          ))}
         </ul>
-      }
+      )}
     </div>
   );
 }
--- src/components/StatusSelect.jsx
+++ src/components/StatusSelect.jsx
@@ -1,9 +1,19 @@
-import { possibleStatus } from "../pages/Issues";
-
+const possibleStatus = [
+  { id: "backlog", label: "Backlog" },
+  { id: "todo", label: "To-do" },
+  { id: "inProgress", label: "In Progress" },
+  { id: "done", label: "Done" },
+  { id: "cancelled", label: "Cancelled" },
+];
 export function StatusSelect({ value, onChange }) {
   return (
-    <select value={value} onChange={onChange} className='status-select'>
-      <option value=''>Select a status to filter</option>
-      {possibleStatus.map(status => <option value={status.id} key={status.id}>{status.label}</option>)}
-    </select>);
+    <select value={value} onChange={onChange} className="status-select">
+      <option value="">Select a status to filter</option>
+      {possibleStatus.map((status) => (
+        <option value={status.id} key={status.id}>
+          {status.label}
+        </option>
+      ))}
+    </select>
+  );
 }
