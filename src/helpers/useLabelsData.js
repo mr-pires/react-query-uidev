74fde36a4d63210a7aed43758f731cddd795b736
mr-pires <piresrodriguesmiguel@gmail.com>
2022-11-03 18:32:02 +0000

    react-devtools

--- src/helpers/useLabelsData.js
+++ src/helpers/useLabelsData.js
@@ -1,9 +1,9 @@
-import { useQuery } from "react-query";
+import { useQuery } from 'react-query';
 
 export function useLabelsData() {
-  const labelsQuery = useQuery(["labels"], () =>
-    fetch("/api/labels").then((res) => res.json())
-  );
+  const labelsQuery = useQuery(['labels'], () => fetch('/api/labels').then((res) => res.json()), {
+    staleTime: 1000 * 60 * 60,
+  });
 
   return labelsQuery;
 }
--- src/helpers/useUserData.js
+++ src/helpers/useUserData.js
@@ -1,9 +1,9 @@
-import { useQuery } from "react-query";
+import { useQuery } from 'react-query';
 
 export function useUserData(userId) {
-  const usersData = useQuery(["users", userId], () =>
-    fetch(`/api/users/${userId}`).then((res) => res.json())
-  );
+  const usersData = useQuery(['users', userId], () => fetch(`/api/users/${userId}`).then((res) => res.json()), {
+    staleTime: 1000 * 60 * 5,
+  });
 
   return usersData;
 }
